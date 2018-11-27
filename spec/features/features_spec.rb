4c56041b42efc944ffd78ea6a084e93f38a9a1c8
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-11-27 12:40:48 +0000

    refactored feature_spec test for '/' route

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -2,7 +2,7 @@ feature 'Feature Tests' do
 
   scenario 'User navigates to the index page' do
     visit ('/')
-    expect(page).to have_content('Chitter Index Page')
+    expect(page).to have_content('Page Under Construction')
   end
 
   scenario 'User navigagtes to the peeps page' do
